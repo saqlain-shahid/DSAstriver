4f6894c13b692a2dddebd8568eba33ea38737dc7
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-02-14 16:10:05 +0530

    Update CountDigit.java

--- src/CountDigit.java
+++ src/CountDigit.java
@@ -1,12 +1,15 @@
 public class CountDigit {
     public static void main(String[] args) {
-        int count = 0;
+//        int count = 0;
         int num = 8353;
-        while(num>0){
-            int last = num % 10;
-            count++;
-            num/=10;
-        }
-        System.out.println(count);
+//        while(num>0){
+//            int last = num % 10;
+//            count++;
+//            num/=10;
+//        }
+//        System.out.println(count);
+        int cnt = (int) Math.log10(num) + 1;
+        System.out.println(cnt);
     }
+
 }
