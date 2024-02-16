0dd6f4a7235b16dd28c0e8ebb5741def91e9e754
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-02-16 23:59:29 +0530

    Update ArrayQues.java

--- src/ArrayQues.java
+++ src/ArrayQues.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ArrayQues {
     public static void main(String[] args) {
         //largest element in an array
-        int[] arr = {1,5,7,3,8};
+        int[] arr = {1,5,7,3,8,9,98};
         System.out.println(largestElem(arr));
+        System.out.println(secondLargest(arr));
 
     }
     static int largestElem(int[] arr){
@@ -13,5 +17,22 @@ public class ArrayQues {
             }
         }
         return lar;
+    }
+
+
+    static int secondLargest(int[] arr){
+       int high = Integer.MIN_VALUE;
+       int secHigh = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > high){
+                secHigh = high;
+                high = arr[i];
+            }
+            if (arr[i] < high && arr[i] > secHigh){
+                secHigh = arr[i];
+            }
+        }
+        return secHigh;
     }
 }
