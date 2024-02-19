fbe130b0901fb3077f21fd774d2eaa87ee5ed1b2
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-02-19 21:56:33 +0530

    Update SortArrayNonDecOrder.java

--- src/SortArrayNonDecOrder.java
+++ src/SortArrayNonDecOrder.java
@@ -1,46 +1,36 @@
 import java.util.Arrays;
-import java.util.Collections;
 
 public class SortArrayNonDecOrder {
-    //code need some changess   ...
     public static void main(String[] args) {
-        int[] arr = {-8,-2,5,4,5,-12};
-       int ans[] = sort(arr);
-       System.out.println( Arrays.toString(ans));
+        int[] arr = {-8, -2, 5, 4, -12};
+        int[] ans = sort(arr);
+
+        // Print the sorted array 'ans'
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
+        }
     }
-    static int[] sort(int[] arr){
+
+    static int[] sort(int[] arr) {
         int n = arr.length;
         int left = 0;
-        int right = n-1;
-        int k =0;
+        int right = n - 1;
+        int k = n - 1;
         int[] ans = new int[n];
 
-        while(left<=right){
-            if(Math.abs(arr[left]) > Math.abs(arr[right])){
-                ans[k++] = arr[left] * arr[left];
+        while (left <= right) {
+            if (Math.abs(arr[left]) > Math.abs(arr[right])) {
+                ans[k--] = arr[left] * arr[left];
                 left++;
-            }
-            else{
-                ans[k++] = arr[right] * arr[right];
+            } else {
+                ans[k--] = arr[right] * arr[right];
                 right--;
             }
         }
-        reverse(ans);
+
+        // Sort the array of squared values
+        Arrays.sort(ans);
+
         return ans;
     }
-    static void reverse(int[] arr){
-        int i =0, j=arr.length-1;
-        while (i<j){
-            swap(arr,i,j);
-            i++;
-            j--;
-        }
-
-    }
-    static void swap(int[] arr, int l, int r){
-        int temp = arr[l];
-        arr[l] = arr[r];
-        arr[r] = temp;
-    }
-
 }
