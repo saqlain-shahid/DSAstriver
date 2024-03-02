32750e951bc066ac76a0f933eb55fdca728be10d
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-03-02 19:04:38 +0530

    Update PowerBase.java

--- src/Recursion/PowerBase.java
+++ src/Recursion/PowerBase.java
@@ -2,7 +2,12 @@ package Recursion;
 
 public class PowerBase {
     public static void main(String[] args) {
-
+        System.out.println(pow(5,4));
     }
-    
+    static int pow(int m, int n){
+        if (n==0){
+            return 1;
+        }
+        return pow(m,n-1) * m;
+    }
 }
