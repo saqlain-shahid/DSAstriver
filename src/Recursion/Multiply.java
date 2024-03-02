c2524963fdb36c9e4bac0ed2b5f3ab607eaa7a7d
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-03-03 00:47:27 +0530

    added codes

---
+++ src/Recursion/Multiply.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Multiply {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        mult(n,k);
+    }
+    static void mult(int n, int k){
+        if (k==1){
+            System.out.println(n);
+            return;
+        }
+        mult(n,k-1);
+        System.out.println(n*k);
+    }
+}
--- src/Recursion/PowerBase.java
+++ src/Recursion/PowerBase.java
@@ -2,12 +2,20 @@ package Recursion;
 
 public class PowerBase {
     public static void main(String[] args) {
-        System.out.println(pow(5,4));
+        System.out.println(pow2(5,3));
     }
     static int pow(int m, int n){
         if (n==0){
             return 1;
         }
         return pow(m,n-1) * m;
+    }
+    //2ns approach
+    static int pow2(int p, int q){
+        if (q==0) return 1;
+        int smallPow = pow2(p,q/2);
+        if (q%2==0) return smallPow * smallPow;
+
+        return p * smallPow * smallPow;
     }
 }
