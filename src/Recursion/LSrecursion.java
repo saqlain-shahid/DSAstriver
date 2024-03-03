b8d72c7469a34e519fcd83127542eb9019f22599
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-03-03 21:35:05 +0530

    Update LSrecursion.java

--- src/Recursion/LSrecursion.java
+++ src/Recursion/LSrecursion.java
@@ -1,4 +1,16 @@
 package Recursion;
 
 public class LSrecursion {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,5};
+        System.out.println(linearSearch(arr,9,0));
+    }
+    static boolean linearSearch(int[] arr, int target, int idx){
+        //base case
+        if (idx >= arr.length) return false;
+        //self work
+        if (target == arr[idx]) return true;
+        // recursive work
+        return linearSearch(arr, target, idx + 1);
+    }
 }
