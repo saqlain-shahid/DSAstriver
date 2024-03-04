6ee09f771f1c3cbdc65f39a6fe1e30d37ff0ed80
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-03-04 14:57:59 +0530

    Update LSrecursion.java

--- src/Recursion/LSrecursion.java
+++ src/Recursion/LSrecursion.java
@@ -1,10 +1,16 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class LSrecursion {
     public static void main(String[] args) {
         int[] arr = {1,2,2,2,2,3,5,2,2,2,2,2};
 //        System.out.println(linSearch(arr,3,0));
-        printAllIndices(arr,2,0);
+//        printAllIndices(arr,2,0);
+
+        ArrayList<Integer> ans = allIndices(arr,2,0);
+        System.out.println(ans);
+
     }
     static boolean linearSearch(int[] arr, int target, int idx){
         //base case
@@ -25,5 +31,19 @@ public class LSrecursion {
         if (arr[idx]==target) System.out.println(idx);
 
          printAllIndices(arr,target,idx+1);
+    }
+
+    static ArrayList<Integer> allIndices(int[] arr, int target, int idx){
+        ArrayList<Integer> ans = new ArrayList<>();
+        //base case
+        if (idx>=arr.length) return ans; //empty arrayList
+        //self work
+        if (arr[idx]==target) { // ans-> {0}
+            ans.add(idx);
+        }
+        //recursive work
+        ArrayList<Integer> smallAns = allIndices(arr,target, idx+1);
+        ans.addAll(smallAns);  // all indixes combined
+        return ans;
     }
 }
